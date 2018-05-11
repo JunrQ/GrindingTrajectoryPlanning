95b466e3253bb7440ab2155adbaeefca9e334f34
JunrQ <137556741@qq.com>
2018-05-11 23:15:28 +0800

    fix bugs

--- generatePathFromClusters.m
+++ generatePathFromClusters.m
@@ -2,6 +2,9 @@
 %
 % path = generatePathFromClusters(clusters, v, f, n), generate path
 % after clusters generated.
+%
+%Author::
+% - JunrZhou
 function path = generatePathFromClusters(clusters, v, f, n, gap, method)
 if nargin < 6
     method = 0;
@@ -11,25 +14,26 @@ if nargin < 5
 end
 [~, cluster_num] = size(clusters);
 pathInfoCell = {};
+endPoints = zeros(cluster_num, 6);
 for cluIdx=1:cluster_num
     [pointsCloud, pointsCloudFaceIdx] = generatePointsCloud(clusters{cluIdx}, v, f, n, gap);
     % use graph traverse to reorder
     [orderedPointsCloud, orderedPointsCloudIdx] = myTraverser(pointsCloud, pointsCloudFaceIdx, method);
     [tmp, ~] = size(orderedPointsCloud);
     % save information
-    pathInfoCell{cluIdx, 1} = orderedPointsCloud;
-    pathInfoCell{cluIdx, 2} = orderedPointsCloudIdx;
-    pathInfoCell{cluIdx, 3} = orderedPointsCloud(1, :);
-    pathInfoCell{cluIdx, 4} = orderedPointsCloud(tmp, :);
+    pathInfoCell{cluIdx, 1} = orderedPointsCloud; % points coordinate
+    pathInfoCell{cluIdx, 2} = orderedPointsCloudIdx; % index of face
+    pathInfoCell{cluIdx, 3} = orderedPointsCloud(1, :); % start point
+    pathInfoCell{cluIdx, 4} = orderedPointsCloud(tmp, :); % end point
+    endPoints(cluIdx, 1:3) = orderedPointsCloud(1, :);
+    endPoints(cluIdx, 4:6) = orderedPointsCloud(tmp, :);
 end
 
 % use cluster index and start, end point of the cluster as features
 % use greedy algorithm to reorder
-cluOrder = ones(cluster_num, 1);
-for i=1:cluster_num
-    
-end
+clusterOrder = myPrim(endPoints);
 
+% generate path
 
 
 end
--- myPrim.m
+++ myPrim.m
@@ -1,61 +1,33 @@
 %MYPRIMFORLINES
-% prim for connect lines
 %
-function [reordered, r_norms] = myPrimForLines(lines)
+% reordered = myPrimForLines(lines) prim for connect lines, this is very simple implementation.
+%
+%Author::
+% - JunrZhou
+
+function reordered = myPrim(lines)
 % chose a line randomly
 [m, n] = size(lines);
-
-considered_l_idx = zeros(1, m);
-
-% find the max z as start point
-[~, tmp_i] = max(lines(:, [3 6]));
-% root = ceil(rand() * m);
-root = max(tmp_i);
-
-cur_line_idx = root;
-
-% add the first line
-cur_line = lines(cur_line_idx, :);
-reordered = lines(cur_line_idx, :);
-r_norms = norms(cur_line_idx, :);
-considered_l_idx(cur_line_idx) = 1;
-
+reordered = zeros(m, 1);
+visited = zeros(m, 1);
+visited(1) = 1;
+minIdx = 1;
 % travser
-for i = 1:(m-1)
-    % which side
-    side_indi = zeros(1, m);
-    length_indi = ones(1, m) * 2147483647;
-    
+for i = 2:m
+    minLength = realmax;
     for j = 1:m
         % if considered, skip it
-        if considered_l_idx(j) == 1
+        if (abs(visited(j) - 1) < 1e-4)
             continue
         end
-        tar_line = lines(j, :);
-        tmp_dist = [norm(cur_line(4:6) - tar_line(1:3)),...
-                    norm(cur_line(4:6) - tar_line(4:6))];
-        
-        [tmp_min, tmp_min_idx] = min(tmp_dist);
-        side_indi(j) = tmp_min_idx;
-        length_indi(j) = tmp_min;
-
+        tmp_dist = norm(minIdx(j, 4:6) - lines(j, 1:3));
+        if tmp_dist < minLength
+            minIdx = j;
+            minLength = tmp_dist;
+        end
     end
-    % connect one
-    [~, tmp_min_idx] = min(length_indi);
-    % see which side
-    side_num = side_indi(tmp_min_idx);
-    % corrs to tmp_dist
-    if side_num == 2
-        cur_line = [lines(tmp_min_idx, 4:6), lines(tmp_min_idx, 1:3)];
-    else
-        cur_line = lines(tmp_min_idx, :);
-    end
-    reordered = [reordered; cur_line];
-    r_norms = [r_norms; norms(tmp_min_idx, :)];
-    cur_line_idx = tmp_min_idx;
-    considered_l_idx(cur_line_idx) = 1;
+    visited(minIdx) = 1;
+    reordered(i) = minIdx;
 end
-
-
 end
 
