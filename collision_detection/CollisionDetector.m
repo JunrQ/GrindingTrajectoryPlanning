8d97a2c78c86e096d6b84b4b7883c2335c4af134
JunrQ <137556741@qq.com>
2018-05-20 00:02:35 +0800

    add detector and test3 and fix bugs

--- collision_detection/CollisionDetector.m
+++ collision_detection/CollisionDetector.m
@@ -1,6 +1,3 @@
-%
-%
-
 classdef CollisionDetector
 properties
     stlPath = "/Users/junr/Documents/Works/graduation-project/code/face_traj/gongjian.stl";
@@ -12,8 +9,8 @@ end
 
 methods
     function cd = CollisionDetector(stlPath)
-        if nargin = 1
-            cd.stlPath = "/Users/junr/Documents/Works/graduation-project/code/face_traj/gongjian.stl";
+        if nargin == 1
+            cd.stlPath = stlPath;
         end
         [cd.v, cd.f, cd.n] = readStlModel(cd.stlPath, @connectPathPreprocessSTLVFN);
         cd.centerP = getCenterPointsOfFace(cd.v, cd.f);
@@ -21,15 +18,57 @@ methods
 end
 
 methods
-    function [nums, points] = traj(obj, lastP, curP, lastN, curN)
-        if nargin == 4
-            while 1
-                
-            end
+    function [nums, points] = traj1(obj, lastP, curP, lastN, curN, l, ps, ns)
+    %TRAJ
+    %
+    %Params::
+    % - lastP, start position, x, y, z
+    % - curP, end position, x, y, z
+    % - lastN, start normal vector
+    % - curN, end normal vector
+    % - ps, [m, 3] all points
+    % - ns, [m, 3] all normal vectors
+    tmpCount = 1;
+    tmpLength = ceil(norm(lastP - curP) * 1.5);
+    tmpPoints = zeros(tmpLength, 3);
+    tmpP = lastP;
+    [numPoints, ~] = size(ps);
+
+    psCells = mat2cell(ps, ones(numPoints, 1), 3);
+    repFlag = tmpP;
+    tmpPoints(1, :) = lastP + l * lastN;
+    while 1
+        if norm(tmpP - curP) < 5
+            break
         end
 
-            
+        d = cellfun(@(x) norm(x-tmpP), psCells);
+        [d_, idx] = sort(d);
+
+        nearest = ps(idx(1:20), :);
+        tmpCell = mat2cell(nearest, ones(20, 1), 3);
+        dT = cellfun(@(x) norm(x-curP), tmpCell);
+
+        [Y, I] = min(dT);
+
+        tmpP = ps(idx(I), :);
+        if all(repFlag == tmpP)
+            break
+        end
+        repFlag = tmpP;
+        tmpCount = tmpCount + 1;
+        if (tmpCount + 1) >= tmpLength
+            tmp = zeros(2 * tmpLength, 3);
+            tmp(1:tmpCount, :) = tmpPoints(1:tmpCount, :);
+            tmpPoints = tmp;
+            tmpLength = 2 * tmpLength;
+        end
+        tmpPoints(tmpCount, :)  = tmpP + l * ns(idx(I), :);
+
+    end
+    tmpPoints(tmpCount + 1, :) = curP + l * curN;
+    nums = tmpCount + 1;
+    points = tmpPoints(1:(tmpCount + 1), :);
     end
 end
-    
 end
---
+++ test3.m
@@ -0,0 +1,39 @@
+% Differences from test2.m
+% 1. add detector
+
+clc
+clear
+tic
+
+%% add pth
+addpath . ./utils/ ./pose/ ./traj/ ./collision_detection/
+
+%% read target model, stl
+% [v, f, n] = readStlModel("/Users/junr/Documents/Works/graduation-project/code/planning/123.stl");
+stlPath = "/Users/junr/Documents/Works/graduation-project/code/face_traj/gongjian.stl";
+[v, f, n] = readStlModel(stlPath);
+
+%% clusters
+clusters = divideIntoFaces(v, f ,n);
+
+%% get path and normal vectors
+[pointsPath, pointsPathIdx, clustersIdx] = generatePathFromClusters(clusters, v, f, n, 2, 0);
+normalVecs = n(pointsPathIdx, :);
+normalVecsM = modifyNormalVec(normalVecs);
+%% after modify normal vectors, all the normal vectors points to inside
+% and closer to z-
+% so we will use normalVecs as original vectors, which points to outside.
+
+%% connect different clusters
+detector = CollisionDetector(stlPath);
+[Ts, conInfo] = connectPaths(pointsPath, normalVecsM, clustersIdx, detector);
+
+%% get arm robot model and do inverse mech
+[myRobot, q0, speed_limit, qlimit] = getRobotModel();
+qs = Ts2q(myRobot, q0, 2, Ts, conInfo, true);
+array2txt(qs, '2018-5-20_t1');
+
+% myRobot.plot(qs(:, 1:6))
+
+toc
+% disp(['Running time: ',num2str(toc)]);
